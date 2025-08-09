dc991f9ebaa26c1ce484f1c5e4da330ac7167c88
caio carvalho <caio.oliveira@vitalsis.com.br-at-950716516331>
2025-08-09 11:28:09 -0300

    feat: cadastro de tipo de despesa e receita

--- src/app/core/service/dominio.service.ts
+++ src/app/core/service/dominio.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TipoDespesa } from '../../shared/models/tipo-despesa.model';
+import { TipoReceita } from '../../shared/models/tipo-receita.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,15 @@ import { TipoDespesa } from '../../shared/models/tipo-despesa.model';
 export class DominioService {
   private readonly http = inject(HttpClient);
 
+  public getDominios(): Observable<TipoDespesa[]> {
+    return this.http.get<TipoDespesa[]>('http://localhost:3000/dominios');
+  }
+
   public inserirTipoDespesa(tipoDespesa: TipoDespesa): Observable<TipoDespesa> {
     return this.http.post<TipoDespesa>('http://localhost:3000/tipoDespesa', tipoDespesa);
   }
 
-  public inserirTipoReceita(tipoReceita: TipoDespesa): Observable<TipoDespesa> {
-    return this.http.post<TipoDespesa>('http://localhost:3000/tipoReceita', tipoReceita);
+  public inserirTipoReceita(tipoReceita: TipoReceita): Observable<TipoReceita> {
+    return this.http.post<TipoReceita>('http://localhost:3000/tipoReceita', tipoReceita);
   }
 }
--- src/app/features/dominios/dominio/dominio.component.ts
+++ src/app/features/dominios/dominio/dominio.component.ts
@@ -3,6 +3,11 @@ import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators 
 import { Button } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { Select } from 'primeng/select';
+import { DominioService } from '../../../core/service/dominio.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { finalize } from 'rxjs';
+import { TipoDespesa } from '../../../shared/models/tipo-despesa.model';
+import { TipoReceita } from '../../../shared/models/tipo-receita.model';
 
 @Component({
   selector: 'app-dominio',
@@ -12,26 +17,51 @@ import { Select } from 'primeng/select';
 })
 export class DominioComponent {
   private readonly fb = inject(FormBuilder);
+  private readonly dominioService = inject(DominioService);
 
   public readonly form = this.fb.nonNullable.group({
     descricao: ['', Validators.required],
     tipo: new FormControl<number | undefined>(undefined, [Validators.required]),
   });
 
-  public readonly tipos = signal([
-    {
-      id: 1,
-      nome: 'Tipo de despesa'
-    }, {
-      id: 2,
-      nome: 'Tipo de receita'
-    }
-  ]);
-
+  public readonly tipos = toSignal(this.dominioService.getDominios().pipe(finalize(() => this.loading.set(false))));
   public readonly loading = signal(true);
 
   public inserir(): void {
-
+    const tipo = this.form.value.tipo;
+    if (tipo === 1) {
+      this.inserirTipoDespesa();
+    }
+    if (tipo === 2) {
+      this.inserirTipoReceita();
+    }
   }
 
+  private inserirTipoDespesa(): void {
+    const despesa: TipoDespesa = {
+      id: 0,
+      nome: this.form.value.descricao!
+    }
+    this.dominioService.inserirTipoDespesa(despesa).subscribe({
+      next: () => {
+        this.resetForm();
+      }
+    });
+  }
+
+  private inserirTipoReceita(): void {
+    const receita: TipoReceita = {
+      id: 0,
+      nome: this.form.value.descricao!
+    }
+    this.dominioService.inserirTipoReceita(receita).subscribe({
+      next: () => {
+        this.resetForm();
+      }
+    });
+  }
+
+  private resetForm(): void {
+    this.form.reset();
+  }
 }
